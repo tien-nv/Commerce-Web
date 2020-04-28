81955b89c696302f0cef548676a63cf5c77bad31
tien-nv <tien.n.van1999@gmail.com>
2020-04-28 18:46:03 +0700

    add security for register

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -21,8 +21,8 @@ class LoginController extends Controller
             $password = $_COOKIE['password_cw'];
             $userName = decrypt($userName);
             $password = decrypt($password); // ra plain text
-            
-            $password = hash("ripemd160",$password);
+
+            $password = hash("ripemd160", $password);
             //gọi đến hàm kiểm tra
             $check = accountProcess::checkUserLogin($userName, $password);
             // nếu hợp lệ
@@ -66,17 +66,38 @@ class LoginController extends Controller
     {
         $email = $request->input('emailRegister');
         $userName = $request->input('userRegister');
-        $password = $request->input('passwordRegister');
+        $password = $request->input('passwordRegister'); //password plaintext
         $phone = $request->input('phoneRegister');
         $userRole = $request->input('sel1');
         $address = $request->input('address');
 
 
         //mã hóa password người dùng rồi luuwu vào database để đảm bảo quyền riêng tư
-        $password = hash('ripemd160', $password);
+        $password_hash = hash('ripemd160', $password); //password hash để lưu vào database
         $query = new QueryDB();
-        $resultRegister = $query->addUser($userRole, $userName, $password, $address, $email, $phone);
+        $resultRegister = $query->addUser($userRole, $userName, $password_hash, $address, $email, $phone);
         if ($resultRegister) {
+            //set session
+            session_start();
+            $_SESSION['userName_cw'] = $userName;
+            $_SESSION['password_cw'] = $password_hash; //password này đã được mã hóa hash
+
+            //mã hóa tên các trường
+            $cookie_username = 'userName_cw';
+            $cookie_password = 'password_cw';
+            // $cookie_username = encrypt($cookie_username);
+            // $cookie_password = encrypt($cookie_password);
+            //mã hóa username và password
+            $userName_encry = encrypt($userName);
+            $password_encry = encrypt($password);
+
+            //đặt cookies là cái mã hóa
+            setcookie($cookie_username, $userName_encry, time() + (86400 * 30)); //1 day
+            //mật khẩu
+
+            setcookie($cookie_password, $password_encry, time() + (86400 * 30)); //1 day
+
+            //trả về tên dạng plaintext
             //nếu thành công thì dadwndg nhập và hiện thị thông báo
             return view('home', compact('userName', 'resultRegister'));
         } else {
@@ -98,7 +119,7 @@ class LoginController extends Controller
     {
         $userName = $request->input('usernameLogin');
         $password_plaintext = $request->input('passwordLogin');
-        $password = hash("ripemd160",$password_plaintext);
+        $password = hash("ripemd160", $password_plaintext);
         $check = accountProcess::checkUserLogin($userName, $password);
         //nếu khong hợp lệ thì trả về thông báo khoogn hợp lệ
         if ($check === false) return view('home', compact('check'));
@@ -121,7 +142,7 @@ class LoginController extends Controller
         //đặt cookies là cái mã hóa
         setcookie($cookie_username, $userName_encry, time() + (86400 * 30)); //1 day
         //mật khẩu
-        
+
         setcookie($cookie_password, $password_encry, time() + (86400 * 30)); //1 day
 
         //trả về tên dạng plaintext
