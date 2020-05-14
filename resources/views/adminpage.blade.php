37a07f3f460a8ba20a287d7eb9ea8d1dc0b14a0e
0pdo1Tall <43989436+0pdo1Tall@users.noreply.github.com>
2020-05-14 20:57:11 +0700

    fix admin front-end (#6)

--- resources/views/adminpage.blade.php
+++ resources/views/adminpage.blade.php
@@ -3,7 +3,11 @@
 @section('title','Admin Pornhub of VietNamese')
 <!-- thêm thư viện -->
 @section('addlib')
-<script type="text/javascript" src="js\\admin.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="bin\\bootstrap441\\css\\bootstrap.min.css">
+    <script src="bin\\jquery341\\jquery.min.js"></script>
+    <script src="bin\\bootstrap441\\js\\bootstrap.min.js"></script>
+    <script type="text/javascript" src="js\\admin.js"></script>
 @endsection
 
 @section('main')
